cd53c71acadc139bbbe2fff0aabc96635135f99c
Andrey Znak <a.y.znak@gmail.com>
2020-09-28 20:14:14 +0300

    feat: add dicision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined || array.length === 0) return 0; 
+  return array.sort((a, b) => a - b)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == undefined || array.length === 0) return 0; 
+  return array.sort((a, b) => a - b)[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == undefined || array.length === 0) return 0; 
+  let sum = array.reduce((current, sum) => current + sum, 0)
+  return sum / array.length;
 }
